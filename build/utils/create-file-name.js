0970ca1d226112ffc4cc2ef2bd1a31878c7db466
kalush81 <krzysiek.kaluza@gmail.com>
2022-06-02 13:47:08 +0200

    Remove build dir

--- build/utils/create-file-name.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createFileName = void 0;
-function createFileName(obj) {
-    return Object.values(obj).join(",").replaceAll(",", "").concat(".jpg");
-}
-exports.createFileName = createFileName;
